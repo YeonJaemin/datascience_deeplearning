947c5c3a34a90f5ecae6f7a72727e08f2450619b
ssojung <ssojung.p@gmail.com>
2017-04-17 10:00:32 +0900

    skeleton :)

---
+++ tensorflow_xor.py
@@ -0,0 +1,31 @@
+import numpy as np
+import tensorflow as tf
+
+"""
+phase1. assemble a graph
+"""
+#load data 'xor_dataset.txt'
+
+#define placeholders for input and output
+
+#define the weights and bias
+
+#define hypotheses
+
+#define loss function
+
+#define optimizer
+
+
+
+"""
+#phrase2. use a session to execute operations in the graph
+"""
+with tf.session() as sess:
+	#init variables
+	#wirte your own code...
+
+	#test trained model
+
+	#check accuracy
+	
